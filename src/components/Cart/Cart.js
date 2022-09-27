1f1ee78d51846d9ec4df389aac50c8d572327792
Sadia88 <sadiasharmin460280@gmail.com>
2022-09-27 23:03:35 +0600

    added new component in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,37 @@
 import React from 'react';
-
+import './Cart.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDeleteLeft } from '@fortawesome/free-solid-svg-icons'
 const Cart = ({cart}) => {
+    console.log(cart)
+    let total=0;
+    let shipping=0;
+    for(const product of cart){
+        total=total+product.price
+        shipping=shipping+product.shipping;
+       
+    }
+    
+   const tax=(total*0.1).toFixed(2)
     return (
-        <div>
+        <div className='cart'>
             <h4>Order summary</h4>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price: ${cart.price}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping Charge: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h4>Grand Total: {}</h4>
+
+            <div>
+                <button>
+                    <p>Clear Cart</p>
+                   
+                    
+                </button>
+                <button>
+                    <p></p>
+                </button>
+            </div>
         </div>
     );
 };
